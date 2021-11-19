c869c99f557ce4a94ca89f66177865ffbddc9655
Nabendu Biswas <nabendu.biswas@gmail.com>
2021-11-19 12:33:51 +0530

    Final commit

---
+++ src/components/ProductListing.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import ProductComponent from './ProductComponent';
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "../redux/actions/productsActions";
+
+const ProductListing = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
+
+    return (
+        <div className="ui grid container">
+            <ProductComponent />
+        </div>
+    )
+}
+
+export default ProductListing
